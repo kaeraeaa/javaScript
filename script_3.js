1bc2c3e7c79e09c26445aa9bb8c6469e4d29c7ba
kaeraeaa <129053421+kaeraeaa@users.noreply.github.com>
2024-01-05 11:43:43 +0200

    comit

---
+++ script_3.js
@@ -0,0 +1,29 @@
+// 1
+const result = 5 + 5 + '5';
+console.log(result);
+
+
+
+// 2
+const email = "kaeraea@gmail.com";
+const symbol = email.includes('@');
+
+console.log("symbol @ :", symbol);
+
+
+//3
+const word1 = "My";
+const word2 = "name";
+const word3 = "is";
+
+const fullName = word1 + ' ' + word2 + ' ' + word3 + ' ' + 'Viktor';
+
+console.log(fullName);
+
+
+//4
+const userName = "Олександр";
+const payment = 300;
+const message = `Дякуємо, ${userName}! До сплати ${payment} гривень`;
+
+alert(message);
